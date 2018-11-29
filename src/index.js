46f6c972c00fa8b5f1b5db6b97b81e62b430b211
KLPainter <KLPainter@gmail.com>
2018-11-29 09:36:33 -0800

    tests are running at the right time

--- src/index.js
+++ src/index.js
@@ -10,19 +10,24 @@ export function isPromise(possiblePromise) {
 export const middleware = store => next => action => {
 
   if(!isPromise(action.payload)) {
-    next(action);
+    console.log('calling next on non-promise action', action.type);
+    return next(action);
   }
 
+  console.log('dispatching load_start before promise');
   store.dispatch({ type: 'LOAD_START' });
-
-  action
+  action.payload
     .then(results => {
+      console.log('results of promise:', results);
       next({
         type: action.type,
         payload: results
       });
+      console.log('dispatching load_end after promise');
+      store.dispatch({ type: 'LOAD_END' });
     })
     .catch(error => {
+      console.log('dispatching load_end in error');
       store.dispatch({ type: 'LOAD_END' });
       store.dispatch({
         type: 'ERROR',
@@ -30,5 +35,5 @@ export const middleware = store => next => action => {
       });
     });
 
-  store.dispatch({ type: 'LOAD_END' });
+
 };
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,48 @@
-// import redux from 'redux';
-import { isPromise } from './index';
+import redux, { createStore, applyMiddleware } from 'redux';
+import { middleware, isPromise } from './index';
 
-describe ('isPromise test', () => {
+describe ('middleware tests', () => {
+
+  it('responds to a successful promise', () => {
+
+    const reducer = jest.fn();
+    const next = jest.fn();
+    const action = { type: 'SUCCESSFUL PROMISE', payload: Promise.resolve('Howdy') };
+    const store = createStore(reducer, applyMiddleware(middleware));
+
+    middleware(store)(next)(action);
+
+    return action.payload
+      .then(() => {
+        console.log('running tests');
+        expect(next.mock.calls).toHaveLength(1);
+        expect(store.dispatch.calls).toHaveLength(2);
+        // expect(action.mock.calls[0][0]).toBe(store.dispatch);
+        // expect(action.mock.calls[0][1]).toBe(store.getState);
+      });
+
+    console.log('too soon to run tests!');
+
+  });
+
+});
+
+/*
+Create an src/index.test.js file and put your tests inside.
+
+test your middleware
+create a mock reducer with const reducer = jest.fn()
+create a store using the mock reducer const store = createStore(reducer, applyMiddleware(promiseMiddleware))
+Test successful promise (use Promise.resolve as payload)
+LOAD_START action is sent
+LOAD_END action is sent
+PROMISE_ACTION action is sent
+Test unsuccessful promise (use Promise.reject as payload)
+LOAD_END action is sent
+ERROR action is sent
+ */
+
+describe ('isPromise tests', () => {
 
   it('returns true when sent a promise', () => {
     const myPromise = new Promise(function(resolve, reject) {
