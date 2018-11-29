01c4484e74a7e02ae399e0222e8e116e2405e47f
KLPainter <KLPainter@gmail.com>
2018-11-29 11:11:24 -0800

    cleaned up and still working

--- src/index.js
+++ src/index.js
@@ -10,30 +10,25 @@ export function isPromise(possiblePromise) {
 export const middleware = store => next => action => {
 
   if(!isPromise(action.payload)) {
-    console.log('calling next on non-promise action', action.type);
     return next(action);
   }
 
-  console.log('dispatching load_start before promise');
   store.dispatch ({ type: 'LOAD_START' });
   action.payload
     .then(results => {
-      console.log('results of promise:', results);
       next({
         type: action.type,
         payload: results
       });
-      console.log('dispatching load_end after promise');
       store.dispatch({ type: 'LOAD_END' });
     })
     .catch(error => {
-      console.log('dispatching load_end in error');
       store.dispatch({ type: 'LOAD_END' });
-      return store.dispatch({
+      store.dispatch({
         type: 'ERROR',
         payload: error
       });
-      //throw error;
+      throw error;
     });
 
 
